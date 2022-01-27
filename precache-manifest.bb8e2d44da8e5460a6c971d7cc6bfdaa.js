9ecbf6e7bc7a35c72c116132194aa4259496c4be
Sam Tureski <gpiggies@gmail.com>
2022-01-26 19:22:41 -0500

    Updates

---
+++ precache-manifest.bb8e2d44da8e5460a6c971d7cc6bfdaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd411225347b3633558b4e25cfc18e3e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0e2934d4b972afd21252",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "97988814670f813668a5",
+    "url": "/home/static/js/2.11fe5261.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.11fe5261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2934d4b972afd21252",
+    "url": "/home/static/js/main.9bbbc5b3.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "0cb6a2270125e784480ae16851c98b7b",
+    "url": "/home/static/media/resume.0cb6a227.pdf"
+  },
+  {
+    "revision": "64bc58eb31d22a0e080da1792c668600",
+    "url": "/home/static/media/sam.64bc58eb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.bb8e2d44da8e5460a6c971d7cc6bfdaa.js"
 );
 
 self.addEventListener('message', (event) => {
